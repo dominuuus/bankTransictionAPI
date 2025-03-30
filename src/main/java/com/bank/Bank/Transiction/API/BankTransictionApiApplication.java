5b28891a7e6e2163d8e44ffc922c2e18805ecb03
Domingos <dmgssantiago@gmail.com>
2025-03-29 22:43:32 -0300

    Configuração dos arquivos iniciais do projeto

---
+++ src/main/java/com/bank/Bank/Transiction/API/BankTransictionApiApplication.java
@@ -0,0 +1,13 @@
+package com.bank.Bank.Transiction.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankTransictionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankTransictionApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bank/Bank/Transiction/API/BankTransictionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.Bank.Transiction.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankTransictionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
