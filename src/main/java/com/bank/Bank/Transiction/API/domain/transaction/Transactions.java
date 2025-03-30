284ef8b7feed2842e9e41a61c8f483b4aad9280b
Domingos <dmgssantiago@gmail.com>
2025-03-29 23:04:11 -0300

    Criação da entidade de transação e configuração do H2 Database no app properties

---
+++ src/main/java/com/bank/Bank/Transiction/API/domain/transaction/Transactions.java
@@ -0,0 +1,42 @@
+package com.bank.Bank.Transiction.API.domain.transaction;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import com.bank.Bank.Transiction.API.domain.user.User;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name="transactions")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode(of="id")
+public class Transactions {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal amount;
+
+    @ManyToOne
+    @JoinColumn(name="sender_id")
+    private User sender;
+
+    @ManyToOne
+    @JoinColumn(name="receiver_id")
+    private User receiver;
+
+    private LocalDateTime timestamp;
+}
