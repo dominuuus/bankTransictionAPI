5e84bbd1f3d44858c4d3470475a7f10583e422ec
Domingos <dmgssantiago@gmail.com>
2025-03-30 02:07:31 -0300

    Instalação da dependência swagger-ui para documentação de API

--- src/main/java/com/bank/Bank/Transiction/API/BankTransictionApiApplication.java
+++ src/main/java/com/bank/Bank/Transiction/API/BankTransictionApiApplication.java
@@ -3,6 +3,13 @@ package com.bank.Bank.Transiction.API;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(
+	info = @Info(title = "Bank Transaction API", version = "1.0", description = "API para realização de transação bancária entre usuários")
+)
+
 @SpringBootApplication
 public class BankTransictionApiApplication {
 
